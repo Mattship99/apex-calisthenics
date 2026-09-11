6fa2dc0ee6a0d4bd9854da86d17dad4dfc37ab33
Mattship99 <matthewshipley99@gmail.com>
2026-09-10 21:20:51 -0600

    Create constants.js

---
+++ src/data/constants.js
@@ -0,0 +1,15 @@
+// constants.js
+
+export const MASTER_PATHWAYS = [
+  // ... Paste your entire MASTER_PATHWAYS array here exactly as it is ...
+];
+
+export const MOBILITY_RECOVERY_MODULE = [
+  // ... Paste your entire MOBILITY_RECOVERY_MODULE array here ...
+];
+
+export const LADDER_RUNGS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+export const DEFAULT_CIRCUITS = [
+  // ... Paste your entire DEFAULT_CIRCUITS array here ...
+];
